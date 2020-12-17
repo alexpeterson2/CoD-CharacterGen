11ff9ff4573f8d33600568b9ac9a30793158afdc
alexpeterson2 <35349356+alexpeterson2@users.noreply.github.com>
2020-12-17 01:53:36 -0700

    Update

--- CoD Character Maker/Assets/Scritps/CharacterMenu.cs
+++ CoD Character Maker/Assets/Scritps/CharacterMenu.cs
@@ -8,9 +8,19 @@ public class CharacterMenu : MonoBehaviour
 
     public enum MenuState { Bio, Stats, Merits, Equipment };
 
-    public MenuState activeState;
+    public MenuState currentState;
+    public MenuState newState;
     public GameObject hunterMenu;
     public GameObject vampireMenu;
+    public GameObject bioMenu;
+    public GameObject statsMenu;
+    public GameObject meritsMenu;
+    public GameObject equipmentMenu;
+
+    void Awake()
+    {
+        currentState = MenuState.Bio;
+    }
 
     void Start()
     {
@@ -19,15 +29,71 @@ public class CharacterMenu : MonoBehaviour
 
     void Update()
     {
-        switch (activeState)
+        if(newState != currentState)
+        {
+            ChangeMenu();
+        }
+    }
+
+    private void ChangeMenu()
+    {
+        GameObject[] menus = new GameObject[] { hunterMenu, vampireMenu, bioMenu, statsMenu, meritsMenu, equipmentMenu };
+
+        currentState = newState;
+
+        switch (currentState)
         {
             case MenuState.Bio:
+                for (int i=0; i==menus.Length; i++)
+                {
+                    if (menus[i] != bioMenu)
+                    {
+                        menus[i].SetActive(false);
+                    }
+                    else
+                    {
+                        menus[i].SetActive(true);
+                    }
+                }
                 break;
             case MenuState.Stats:
+                for (int i = 0; i == menus.Length; i++)
+                {
+                    if (menus[i] != statsMenu)
+                    {
+                        menus[i].SetActive(false);
+                    }
+                    else
+                    {
+                        menus[i].SetActive(true);
+                    }
+                }
                 break;
             case MenuState.Merits:
+                for (int i = 0; i == menus.Length; i++)
+                {
+                    if (menus[i] != meritsMenu)
+                    {
+                        menus[i].SetActive(false);
+                    }
+                    else
+                    {
+                        menus[i].SetActive(true);
+                    }
+                }
                 break;
             case MenuState.Equipment:
+                for (int i = 0; i == menus.Length; i++)
+                {
+                    if (menus[i] != equipmentMenu)
+                    {
+                        menus[i].SetActive(false);
+                    }
+                    else
+                    {
+                        menus[i].SetActive(true);
+                    }
+                }
                 break;
         }
     }
@@ -42,5 +108,25 @@ public class CharacterMenu : MonoBehaviour
     public void OnVampire()
     {
         m_manager.character.template = Character.Template.Vampire;
+    }
+
+    public void OnBio()
+    {
+        newState = MenuState.Bio;
+    }
+
+    public void OnStats()
+    {
+        newState = MenuState.Stats;
+    }
+
+    public void OnMerits()
+    {
+        newState = MenuState.Merits;
+    }
+
+    public void OnEquipment()
+    {
+        newState = MenuState.Equipment;
     }
 }
