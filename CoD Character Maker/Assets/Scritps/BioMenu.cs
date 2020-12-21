b9834e7b4dea6f33fd2059a5dcf67b84cd8f7111
alexpeterson2 <35349356+alexpeterson2@users.noreply.github.com>
2020-12-21 11:48:49 -0700

    Update

---
+++ CoD Character Maker/Assets/Scritps/BioMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BioMenu : MonoBehaviour
+{
+    private Manager m_manager;
+
+    void Start()
+    {
+        m_manager = FindObjectOfType<Manager>();
+    }
+
+    public void TemplateHandler(int val)
+    {
+        if (val == 1)
+        {
+            m_manager.character.template = Character.Template.Hunter;
+        }
+        else if (val == 2)
+        {
+            m_manager.character.template = Character.Template.Vampire;
+        }
+    }
+}
